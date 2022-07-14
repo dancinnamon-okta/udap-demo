621351a371a350de891a82ac54ba15ae390ca18f
Dan Cinnamon <dan.cinnamon@okta.com>
2022-07-14 02:01:08 -0500

    Minor tweaks and added patient match

--- aws/authorize.js
+++ aws/authorize.js
@@ -4,9 +4,10 @@ const authorizeLib = require('../lib/authorize')
 //Authorize endpoint - AWS implementation.
 //See the authorize library for full details.
 module.exports.authorizeHandler = async (event, context) => {
-	var authorizeResult = await authorizeLib.authorizeHandler(event.queryStringParameters)
+	var authorizeResult = await authorizeLib.authorizeHandler(event.queryStringParameters, event.headers)
 	return {
 		statusCode: authorizeResult.statusCode,
+		headers: authorizeResult.headers,
 		body: JSON.stringify(authorizeResult.body)
 	}
 }
